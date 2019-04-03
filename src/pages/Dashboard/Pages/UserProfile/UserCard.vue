5bb21f581af3dd08830ae352558626a489b165d0
Diego <d.delajaral@gmail.com>
2019-04-02 22:24:40 -0300

    subida de imagen y carba de la misma en el front

--- src/pages/Dashboard/Pages/UserProfile/UserCard.vue
+++ src/pages/Dashboard/Pages/UserProfile/UserCard.vue
@@ -1,12 +1,27 @@
 <template>
   <card class="card-user">
     <div slot="image" class="image">
-      <img src="@/assets/img/bg5.jpg" alt="...">
+      <img
+        src="@/assets/img/bg5.jpg"
+        alt="Avatar Background">
     </div>
     <div>
       <div class="author">
         <a href="#">
-          <img class="avatar border-gray" src="@/assets/img/mike.jpg" alt="..." @click="selectUploadFile">
+          <img
+            v-if="!file.downloadURL"
+            class="avatar border-gray"
+            src="@/assets/img/mike.jpg"
+            alt="Avatar"
+            @click="selectUploadFile" />
+          <img
+            v-else
+            class="avatar border-gray"
+            :src="file.downloadURL"
+            alt="Avatar"
+            @click="selectUploadFile" />
+
+          <p class="loading">{{ progressUpload }}%</p>
           <h5 class="title">{{ name }} {{ lastName }}</h5>
         </a>
         <input
@@ -18,9 +33,6 @@
           :multiple="false"
           @change="detectFiles($event)" />
 
-          <img
-            :src="file.downloadURL"
-            width="100%" />
 
         <p class="description">
           {{ email }}
@@ -61,10 +73,10 @@ export default {
         fileName: "",
         size: null,
         type: null,
-        uploadTask: "",
         uploading: false,
         uploadEnd: false
-      }
+      },
+      uploadTask: ""
     };
   },
   mounted() {
@@ -86,9 +98,10 @@ export default {
     upload(file) {
       this.file.fileName = file.name;
       this.file.uploading = true;
-      this.file.uploadTask = firebaseStorage
+      this.uploadTask = firebaseStorage
         .ref("profileImages/" + file.name)
         .put(file);
+      console.log(this.uploadTask);
     }
   },
   watch: {
@@ -102,12 +115,10 @@ export default {
         },
         null,
         () => {
-          console.log("downloadURL");
           this.uploadTask.snapshot.ref.getDownloadURL().then(downloadURL => {
-            this.uploadEnd = true;
-            this.downloadURL = downloadURL;
-
-            this.$emit("downloadURL", downloadURL);
+            this.file.uploadEnd = true;
+            this.file.downloadURL = downloadURL;
+            // this.$emit("downloadURL", downloadURL);
           });
         }
       );
